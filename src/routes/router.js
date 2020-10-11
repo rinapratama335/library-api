40ea996c463031f76916be535f2fef4fb8765003
irwantowibowotech <irwantowibowotech@gmail.com>
2020-10-11 23:15:43 +0700

    add Users

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const { auth } = require("../middleware/authentication");
 const { register: registration, login } = require("../controllers/auth");
+const { getAllUsers, deleteUser } = require("../controllers/user");
 
 router.post("/register", registration);
 router.post("/login", login);
+
+router.get("/users", auth, getAllUsers);
+router.delete("/user/:id", auth, deleteUser);
 
 module.exports = router;
